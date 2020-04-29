0a9bcd332cf97f529f9d0c71122ebd7c2480de06
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-04-29 18:18:09 +0530

    - Complete all tasks to satify the criter for exercises 2.11 - 2.14

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Input from "./components/Input";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newFilter, setNewFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -34,9 +30,7 @@ const App = () => {
           (person) => person.name.toLowerCase() === newName.toLowerCase()
         )
       ? alert(`${newName} is already in the book!`)
-      : persons.find(
-          (person) => person.number === newNumber
-        )
+      : persons.find((person) => person.number === newNumber)
       ? alert(`${newNumber} is already in the book!`)
       : setPersons(
           persons.concat({
@@ -45,6 +39,15 @@ const App = () => {
           })
         );
   };
+
+  useEffect(
+    () =>
+      axios
+        .get("http://localhost:3001/persons")
+        .then((response) => setPersons(response.data)),
+    []
+  );
+
   return (
     <div>
       <h2>Phone Book</h2>
