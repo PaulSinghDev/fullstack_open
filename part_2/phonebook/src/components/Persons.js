51aceebca084a5fcf7edee5587d4df4032e10566
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-06-16 07:20:27 +0100

    - Line 2: Added import to db services - Lines 7 - 13: Added click handler for the delete button to allow users to delete record - Lines 24 - 28: Added a reference to the clickhandler I just added on line 7

--- part_2/phonebook/src/components/Persons.js
+++ part_2/phonebook/src/components/Persons.js
@@ -1,17 +1,35 @@
 import React from "react";
+import dbService from "../services/db";
+import Button from "./Button";
 
-const Persons = ({ persons, filter }) => (
-  <ul>
-    {persons
-      .filter((person) =>
-        person.name.toLowerCase().includes(filter.toLowerCase())
-      )
-      .map((filteredPerson) => (
-        <li key={filteredPerson.name}>
-          {filteredPerson.name} - {filteredPerson.number}
-        </li>
-      ))}
-  </ul>
-);
+const Persons = ({ persons, filter, updatePeople }) => {
+  
+  const onClickHandler = async (id) => {
+    const person = persons.find(person => person.id === id);
+    if (window.confirm(`Are you sure you wish to delete the entry for ${person.name}`)){
+      dbService.deleteOne(id)
+        .then(() => updatePeople(persons.filter(person => person.id !== id)));
+    }
+  };
+
+  return (
+    <ul>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((filteredPerson) => (
+          <li key={filteredPerson.name}>
+            {filteredPerson.name} - {filteredPerson.number}
+            <Button
+              onClick={() => onClickHandler(filteredPerson.id)}
+              type="submit"
+              text="Delete"
+            />
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default Persons;
