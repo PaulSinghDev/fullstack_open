e1d760882e6609d43dbca30c0ad68d4f729d5420
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-06-17 16:06:22 +0100

    - Line 5: Import the newly made Notification element - Line 6: Import than db services - Line 14: Add state functionality for the Notification message component - Lines 26 - 50: Added a function to handle the removal of entries within the database. Uses the notification component to inform the use if the operation was successful or not and then returns the notification's state back to null in order to remove it from the user's view - Lines 53 - 75: Altered the addRecordHandler function to utilise the newly created Notification component - Lines 77 - 132: Altered the function which handles changing of the user's number/name to incorporate functionality for both events. USes the notification element. - Lines 138 - 145: Changed the non-input handlers to use the new notification system instead of the brassick js alert function - Lines 148,150: Changed both eventualities to use the upadate record handler and pass a variable indicating whether it is changing the name or the number - Line 164: Added call to the Notification component - Lines 176 - 179: Altered the call to the Persons component so it can utilise the newly created removeRecordHandler

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import Input from "./components/Input";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 import dbService from "./services/db";
+import db from "./services/db";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newFilter, setNewFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null);
 
   const nameChangeHandler = (event) => {
     setNewName(event.target.value);
@@ -20,22 +23,111 @@ const App = () => {
     setNewFilter(event.target.value);
   };
 
-  const addRecordHandler = async (record) => {
-    dbService.addOne(record)
-      .then((response) =>
-      setPersons(persons.concat(response))
-    );
+  const removeRecordHandler = (id) => {
+    const person = persons.find(person => person.id === id);
+    if (window.confirm(`Are you sure you wish to delete the entry for ${person.name}`)){
+      dbService.deleteOne(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id));
+          setNotificationMessage({
+            content: `All information related to ${person.name} has been removed from the database.`,
+            fail: false
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        })
+        .catch(error => {
+          setNotificationMessage({
+            content: `It looks as though there has been an error removing ${person.name} from the database`,
+            fail: true
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        });
+    }
   };
 
-  const updateRecordHandler = () => {
-    const message = `${newName} is already in the phone book. Would you like to update the phone number to ${newNumber}?`;    
-    if(window.confirm(message)) {
-      const record = persons.find(person => person.name === newName);
-      record.number = newNumber;
 
-      dbService.updateOne(record);
+  const addRecordHandler = async (record) => {
+    dbService
+      .addOne(record)
+      .then((response) => {
+        setPersons(persons.concat(response));
+        setNotificationMessage({
+          content: `${response.name} was successfully added to the phone book with the number ${response.number}`,
+          fail: false,
+        });
+        setTimeout(() => {
+          setNotificationMessage(null);
+        }, 5000);
+      })
+      .catch((error) => {
+        setNotificationMessage({
+          content: `There seems to have been an error. ${record.name} was not added to the phone book.`,
+          fail: true,
+        });
+        setTimeout(() => {
+          setNotificationMessage(null);
+        }, 5000);
+      });
+  };
 
-      setPersons(persons.map(person => person.id !== record.id ? person : record));
+  const updateRecordHandler = (toUpdate) => {
+    if(toUpdate === 'name') {
+      let record = {...persons.find((person) => person.name === newName)};
+      const message = `${record.name} is already in the phone book. Would you like to update the phone number to ${newNumber}?`;
+      if(window.confirm(message)) {
+        record.number = newNumber;
+        db.updateOne(record)
+        .then(response => {
+          setPersons(persons.map(person => person.id !== record.id ? person : record));
+          setNotificationMessage({
+            content: `The number associated with ${response.name} has been changed to ${response.number}`,
+            fail: false
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        })
+        .catch(error => {
+          setNotificationMessage({
+            content: `It looks as though there has been an error. The number for ${record.name} has not been updated.`,
+            fail: true
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        });
+      };
+    } else {
+      let record = {...persons.find((person) => person.number === newNumber)};
+      const message = `${record.number} is already in the phone book under the name ${record.name}. Would you like to change the name to ${newName}?`;
+      if(window.confirm(message)) {
+        record.name = newName;
+
+        db.updateOne(record)
+        .then(response => {
+          setPersons(persons.map(person => person.id !== record.id ? person : record));
+          setNotificationMessage({
+            content: `The name associated with the number ${response.number} has been changed to ${response.name}`,
+            fail: false
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        })
+        .catch(error => {
+          setNotificationMessage({
+            content: `It looks as though there has been an error. The name associated with ${record.number} has not been updated.`,
+            fail: true
+          });
+          setTimeout(() => {
+            setNotificationMessage(null);
+          }, 5000);
+        });
+      };
     }
   };
 
@@ -43,20 +135,25 @@ const App = () => {
     event.preventDefault();
 
     return !newName
-      ? alert("You didn't specify a name!")
-      : !newNumber
-      ? alert("You didn't enter a phone number!")
-      : persons.find(
+      ? (
+        setNotificationMessage({content: 'It looks like you haven\'t entered a name', fail: true}),
+        setTimeout(() => setNotificationMessage(null), 5000)
+      ) : !newNumber
+      ? (
+        setNotificationMessage({content: 'It looks like you haven\'t entered a number', fail: true}),
+        setTimeout(() => setNotificationMessage(null), 5000)
+      ) : persons.find(
           (person) => person.name.toLowerCase() === newName.toLowerCase()
         )
-      ? updateRecordHandler()
+      ? updateRecordHandler('name')
       : persons.find((person) => person.number === newNumber)
-      ? alert(`${newNumber} is already in the book!`)
+      ? updateRecordHandler('number')
       : addRecordHandler({
           name: newName,
           number: newNumber,
         });
   };
+
 
   useEffect(() => {
     dbService.getAll().then((people) => setPersons(people));
@@ -64,6 +161,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={notificationMessage} />
       <h2>Phone Book</h2>
       <div>
         <h2>Filter Results</h2>
@@ -75,7 +173,11 @@ const App = () => {
         submitHandler={submitHandler}
       />
       <h2>Phone Numbers</h2>
-      <Persons updatePeople={(people) => setPersons(people)} persons={persons} filter={newFilter} />
+      <Persons
+        persons={persons}
+        filter={newFilter}
+        clickHandler={(id) => removeRecordHandler(id)}
+      />
     </div>
   );
 };
