8cc5ba745a6b9e43bf1d2a13ff9d73e50f2b6c4b
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-06-17 16:12:36 +0100

    Changed all functions to actually return the data from the server's response instead of a promise. - Line 6: Added a fake record to test error reporting - Line 7: Concatenated the fake record to the records returned from the server

--- part_2/phonebook/src/services/db.js
+++ part_2/phonebook/src/services/db.js
@@ -3,22 +3,23 @@ const baseUrl = "http://localhost:3001/persons";
 
 const getAll = async () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  const fakeRecord = { id: 999, name: "Not-on Server", number: '07878797865' };
+  return request.then((response) => response.data.concat(fakeRecord));
 };
 
 const addOne = async (newObject) => {
   const request = axios.post(baseUrl, newObject);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
 const deleteOne = async (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
 const updateOne = async (updatedObject) => {
   const request = axios.put(`${baseUrl}/${updatedObject.id}`, updatedObject);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
 export default { getAll, addOne, deleteOne, updateOne };
