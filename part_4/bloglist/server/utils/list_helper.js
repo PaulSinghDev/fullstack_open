2ddcd2c3c2c51c99966357bda083efe6a3bf212c
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-12-20 15:11:41 +0000

    Part 4.4 - add function on lines 5 - 8 which gets the average total likes Part - 4.5: Lines 10 - 22 add function to get the block which as the most likes

--- part_4/bloglist/server/utils/list_helper.js
+++ part_4/bloglist/server/utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item.likes;
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0) / blogs.length;
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (output, current) => {
+    output = current.likes > output.likes ? current : output;
+    return output;
+  };
+  const favorite = blogs.reduce(reducer, blogs[0]);
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
