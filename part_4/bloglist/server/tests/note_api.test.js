ae8a32429cf9f3b1d87310a6adcb8ece5f3395fb
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-12-27 19:24:52 +0000

    Add a test which creates a new blog post, sends it to the server and then gets a list of all blogs and checks whether the new one is there.

--- part_4/bloglist/server/tests/note_api.test.js
+++ part_4/bloglist/server/tests/note_api.test.js
@@ -3,6 +3,7 @@ const app = require("../app");
 const supertest = require("supertest");
 const initialBlogs = require("./blogs");
 const Blog = require("../models/Blog");
+const { init } = require("../app");
 
 /**
  * Create an api variable to test the app's
@@ -14,13 +15,12 @@ const api = supertest(app);
  * Initialise the database;
  */
 beforeEach(async () => {
-    await Blog.deleteMany({});
-    for ( let blog of initialBlogs ) {
-        const newBlog = new Blog(blog);
-        await newBlog.save();
-    }
+  await Blog.deleteMany({});
+  for (let blog of initialBlogs) {
+    const newBlog = new Blog(blog);
+    await newBlog.save();
+  }
 });
-
 
 test("response is json", async () => {
   await api
@@ -31,8 +31,23 @@ test("response is json", async () => {
 
 test("has id field", async () => {
   const blogs = await api.get("/api/blogs");
-  console.log(blogs.body);
   expect(blogs.body[0].id).toBeDefined();
+});
+
+test("can post", async () => {
+  const newBlog = {
+    title: "A Blog From Jest",
+    author: "Jest",
+    likes: 2,
+    url: "https://makingstuffs.co.uk",
+  };
+
+  await api.post("/api/blogs").send(newBlog);
+
+  const blogs = (await api.get("/api/blogs")).body.map((blog) => blog.title);
+  
+  expect(blogs.length).toBe(initialBlogs.length + 1);
+  expect(blogs).toContain("A Blog From Jest");
 });
 
 afterAll(() => {
