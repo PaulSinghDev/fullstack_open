cd982903b22c6f4b6902c64df6253273b318138d
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-12-28 21:29:59 +0000

    4.15:
    -- 1 - 3: Import helpers
    -- 8, 9: Import models
    -- 14 - 44: DB connect and disconnect functions
    -- 24 - 26: Users in db function

--- part_4/bloglist/server/tests/testHelper.js
+++ part_4/bloglist/server/tests/testHelper.js
@@ -1,12 +1,28 @@
+const initialBlogs = require("./blogs");
+const mongoose = require("mongoose");
+const config = require("../utils/config");
 const app = require("../app");
 const supertest = require("supertest");
-const initialBlogs = require("./blogs");
-const Blog = require("../models/Blog");
 
 const api = supertest(app);
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const blogsInDb = async () =>
   (await Blog.find({})).map((blog) => blog.toJSON());
+
+const dbConnect = async () =>
+  await mongoose.connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+const dbClose = async () =>
+  await mongoose.connection.close();
+
+const usersInDb = async () =>
+  (await User.find({})).map((user) => user.toJSON());
 
 const nonExistingId = async () => {
   const newBlog = {
@@ -24,6 +40,9 @@ const nonExistingId = async () => {
 
 module.exports = {
   initialBlogs,
+  usersInDb,
   blogsInDb,
   nonExistingId,
+  dbConnect,
+  dbClose
 };
