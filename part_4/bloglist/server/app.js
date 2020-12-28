6f0a310fbd4ab08176d61b7a65ba4f930b4fa968
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-12-28 21:25:29 +0000

    4.15: -- imported mongoose -- imported new route handlers -- imported the custom middleware file -- impotrd logger and the express-async-errors package
    -- 17 - 26: Move mongoose connection into a middleware
    -- 28 - 32: Implement the following middlewares: users router, blogs router, error handler and unknownendpoint

--- part_4/bloglist/server/app.js
+++ part_4/bloglist/server/app.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const ApiRouter = require("./controllers/api");
-const app = express();
+const mongoose = require("mongoose");
 const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const app = express();
+require("express-async-errors");
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use(middleware.requestLogger);
+app.use(async (req, res, next) => {
+  await mongoose.connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  return next();
+});
 
-app.use("/api", ApiRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
