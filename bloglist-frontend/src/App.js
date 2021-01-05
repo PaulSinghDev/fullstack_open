36c0ac850f10c2b61948b5fad7e4359e7a82220d
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2021-01-05 18:46:38 +0000

    5.1: Added states for the user to login plus to handle the input of a new blog post in the future - conditionally lod the contact form and bloglist depending whether the user is logged in

---
+++ bloglist-frontend/src/App.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      console.log('yo')
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setPassword('')
+      setUsername('')
+    } catch (error) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      })
+    }
+  }
+
+  const handleBlogSubmit = async () => {
+    console.log('new')
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
+
+  const blogList = () => (
+    <>
+      <h2>Blogs</h2>
+      Currently logged in as {user.username}
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={handleBlogSubmit}>
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        <label>Author</label>
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        <label>URL</label>
+        <input
+          type="url"
+          value={url}
+          name="URL"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
+  return (
+    <div>
+      {user ? blogForm() : loginForm()}
+      {user !== null && blogList()}
+    </div>
+  )
+}
+
+export default App
