1b4d4e6c52cf1c5bb24172e4db092a3fd3facd91
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2021-01-10 12:44:25 +0000

    - completed tasks 6.1 and 6.2 -- 6.1 the initial state question is extremely poorly worded and confusing. The test passes with an undefined original state and provides the initial state. However, providing an empty object will not pass the test. I am not sure if you want this to also pass the test or if you want to fail. It would make sens that this fails as passing an empty object to the reducer would mean there is an error with the code in the first place since the original state should always be an actual usable state.

--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -5,22 +5,22 @@ describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
     ok: 0,
-    bad: 0
+    bad: 0,
   }
 
   test('should return a proper initial state when called with undefined state', () => {
     const state = {}
     const action = {
-      type: 'DO_NOTHING'
+      type: 'DO_NOTHING',
     }
-
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
     const action = {
-      type: 'GOOD'
+      type: 'GOOD',
     }
     const state = initialState
 
@@ -29,7 +29,31 @@ describe('unicafe reducer', () => {
     expect(newState).toEqual({
       good: 1,
       ok: 0,
-      bad: 0
+      bad: 0,
     })
+  })
+
+  test('bad is incrementing', () => {
+    const action = {
+      type: 'BAD',
+    }
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({ good: 0, bad: 1, ok: 0 })
+  })
+
+  test('ok is incrementing', () => {
+    // Set the action which will tell the reducer what to do with the state
+    const action = { type: 'OK' }
+    // Set the initial state of the app
+    const state = initialState
+    // Ensure that this object is not changed by the reducer
+    deepFreeze(state)
+    // Get the new state by calling the reducer which will take the original state
+    // and then update it according to the action specified
+    const newState = counterReducer(state, action)
+    //Ensure the new state has been updated
+    expect(newState).toEqual({ good: 0, bad: 0, ok: 1 })
   })
 })
