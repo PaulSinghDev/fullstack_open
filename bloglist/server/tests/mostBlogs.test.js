612153ccc82b02658841a2a60c279755bcd530b4
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2021-01-19 13:03:03 +0000

    Clean directory

--- bloglist/server/tests/mostBlogs.test.js
+++
@@ -1,8 +0,0 @@
-const mostBlogs = require("../utils/list_helper").mostBlogs;
-const blogs = require("./blogs");
-
-test("With full list", () => {
-  const result = mostBlogs(blogs);
-  expect(result.author).toBe("Robert C. Martin");
-  expect(result.blogs).toBe(3);
-});
